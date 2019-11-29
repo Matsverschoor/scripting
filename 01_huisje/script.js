e2214dde5335bffb8749e9c9e603b728bf84a1c7
mats verschoor <mats>
2019-11-29 14:59:31 +0100

    first commit

---
+++ 01_huisje/script.js
@@ -0,0 +1,54 @@
+// maak een variabele voor de canvas
+const canvas = document.getElementById('canvas');
+
+// leg een 2d-context over de canvas
+const context = canvas.getContext('2d');
+
+//maak de canvas schermvullend
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+
+
+
+// hier kun je op de canvas tekenen
+
+context.beginPath()
+context.fillStyle = "grey";
+context.moveTo(300, 100);
+context.lineTo(700, 200);
+context.lineTo(600, 400);
+context.lineTo(200, 300)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "brown";
+context.moveTo(200, 300)
+context.lineTo(200, 500)
+context.lineTo(600, 600)
+context.lineTo(600, 400)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "purple";
+context.moveTo(600, 400)
+context.lineTo(700, 200)
+context.lineTo(800, 300)
+context.closePath();
+context.stroke();
+context.fill()
+
+context.beginPath()
+context.fillStyle = "brown";
+context.moveTo(600, 600)
+context.lineTo(800, 500)
+context.lineTo(800, 300)
+context.lineTo(600, 400)
+context.closePath();
+context.stroke();
+context.fill()
